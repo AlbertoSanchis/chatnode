d21d08e227e41313ff2c03adf644f6583b3ac173
AlbertoSanchis <163111958+AlbertoSanchis@users.noreply.github.com>
2026-01-07 11:19:20 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+console.log('Servidor WebSocket escuchando en puerto 8080');
+
+wss.on('connection', ws => {
+    console.log('Cliente conectado');
+
+    ws.on('message', mensaje => {
+        // Enviar el mensaje a todos los clientes
+        wss.clients.forEach(cliente => {
+            if (cliente.readyState === WebSocket.OPEN) {
+                cliente.send(mensaje.toString());
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Cliente desconectado');
+    });
+});
